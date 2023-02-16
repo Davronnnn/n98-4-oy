cc31526f5476b6ea9c1256c8c3695279e2916342
Davronnn <kifleader@gmail.com>
2023-02-16 16:10:25 +0500

    last

--- js/lesson.js
+++ js/lesson.js
@@ -1,70 +1,14 @@
-const elInput = findElement('#country');
-const template = findElement('#country-template');
-const elCards = findElement('.row');
-const error = findElement('.error-message');
-let countries = [];
+const API_KEY = 'ff1500955d7a8d833b69f2766606a60b';
 
-const renderCountry = (array, parent = elCards) => {
-	parent.textContent = null;
-	const fragment = document.createDocumentFragment();
-
-	const sortedArray = array.sort((a, b) => {
-		if (a.name.official < b.name.official) {
-			return -1;
-		}
-		if (a.name.official > b.name.official) {
-			return 1;
-		}
-	});
-
-	sortedArray.forEach((country) => {
-		const newCard = template.content.cloneNode(true);
-		const img = findElement('img', newCard);
-		const title = findElement('.title', newCard);
-		const population = findElement('.population', newCard);
-
-		title.textContent = country.name.official;
-		img.src = country.flags.png;
-		population.textContent =
-			country.population > 1000000
-				? Math.round(country.population / 1000000) + ' mln'
-				: country.population + ' kishi';
-
-		fragment.appendChild(newCard);
-	});
-
-	elCards.appendChild(fragment);
-};
-
-elInput.addEventListener('change', () => {
-	// loader.style.display = "block"
-	fetch(`https://restcountries.com/v3.1/name/${elInput.value}`)
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-
-			if (data.message) {
-				throw new Error(data.message);
-			}
-			renderCountry(data);
-
-			error.textContent = '';
-			// loader.style.display = "none"
-		})
-		.catch((err) => {
-			console.log(err);
-			error.textContent = 'Bunday mamlakat mavjud emas';
-			// loader.style.display = "none"
-		})
-		.finally(() => {
-			// loader.style.display = "none"
-		});
-});
-
-fetch('https://restcountries.com/v3.1/all')
-	.then((res) => res.json())
+// fetch(
+// 	`https://api.openweathermap.org/data/2.5/weather?q=${'tashkent'}&units=metric&APPID=46351da790226c653537b9628dc20463`
+// )
+// 	.then((json) => json.json())
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
+fetch(`https://cat-fact.herokuapp.com/facts`)
+	.then((json) => json.json())
 	.then((data) => {
-		countries = data;
-		renderCountry(countries);
-		// loader.style.display = "none"
+		console.log(data);
 	});
